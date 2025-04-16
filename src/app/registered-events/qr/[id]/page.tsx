23c0390fb3726f85691f5f05c0733b4be4d7f12b
Nawadha Jadiya <nawadhajadiya@gmail.com>
2025-04-17 00:04:05 +0530

    auth done and QR done(email)

--- src/app/registered-events/qr/[id]/page.tsx
+++ src/app/registered-events/qr/[id]/page.tsx
@@ -1,29 +1,6 @@
-// 'use client';
-
-// import React from 'react';
-// import axios from 'axios'
 
 
-// export default async function QRPage({ params }: { params: { id: string } }) {
-//   const userId = params.id;
-//   const user = await axios.get('api/me')
-//   const userData = user.data
-//   const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${userData.email}`;
-
-//   return (
-//     <div className="bg-gray-800 text-gray-200 min-h-screen flex flex-col items-center justify-center">
-//       <h1 className="text-2xl font-semibold mb-6">Event QR Code</h1>
-//       <div className="bg-white p-4 rounded-lg shadow-md">
-//         <img src={qrCodeUrl} alt="QR Code" className="w-64 h-64" />
-//       </div>
-//       <p className="mt-4 text-sm text-gray-400">
-//         Scan this QR code to mark attendance for the event.
-//       </p>
-//     </div>
-//   );
-// }
-
-import QRCodePage from '@/components/QRCodePage';
+import QRCodePage from '@/components/QRcodePage';
 
 export default function QRPage({ params }: { params: { id: string } }) {
   return <QRCodePage />;
